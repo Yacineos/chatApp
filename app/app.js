3234c0475afbc48d6fa7ee75fc553f5973baa119
Yacineos <96418831+Yacineos@users.noreply.github.com>
2023-12-26 09:28:58 +0100

    very basic functionnal chat app where every client sees what other clients sent

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
-const socket = new WebSocket('ws://localhost:3000');
+const socket = io('ws://localhost:3500');
 
 function sendMessage(e){
     e.preventDefault();//submit the form without reloading the page
     const input = document.querySelector('input'); // select the input element
     if(input.value){ // if the input value is not empty
-        socket.send(input.value); // send the value to the server
+        socket.emit('message' , input.value); // send the value to the server
         input.value = ''; // reset the input value
     }
     input.focus(); // focus on the input field
@@ -15,7 +15,7 @@ document.querySelector('form')
 
 // listen for messages from the server
 
-socket.addEventListener('message',({data}) => {
+socket.on('message',(data) => {
     const li = document.createElement('li');
     li.textContent = data;
     document.querySelector('ul').appendChild(li);
--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
-const ws = require('ws');
-const server = new ws.Server({port: '3000'});
+import {createServer} from "http" ;
+import {Server} from "socket.io"
 
-    server.on('connection', socket =>{
-        socket.on('message', message =>{
-            messageLus = message ;
-            console.log(message);
-            socket.send(`${message}`);
-        })
+const httpServer = createServer();
+
+const io = new Server(httpServer , {
+    cors: {
+        // if we are not hosting the backend on a different domain then the front end , we need to add the front end production URL 
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500","http://127.0.0.1:5500"]
+    }
+})
+
+io.on('connection', socket =>{
+    // print the id of the connected user 
+    console.log(`User ${socket.id} connected`)
+    
+    socket.on('message', data =>{
+        console.log(data);
+        io.emit('message' ,`${socket.id.substring(0,5)}: ${data}`); // send the first 5 chars of the id of the client + the data
     })
+})
+
+httpServer.listen(3500, ()=> console.log('listening on the port 3500'))
 
