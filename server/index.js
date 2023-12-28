d7e35ee97e991ef613a9f3568b444ff3ba3d6bfc
Yacineos <96418831+Yacineos@users.noreply.github.com>
2023-12-28 15:40:32 +0100

    finished the tutorial

--- server/index.js
+++ server/index.js
@@ -1,77 +1,123 @@
 import express from 'express'
-import {Server} from "socket.io"
+import { Server } from "socket.io"
 import path from 'path'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const PORT = process.env.port || 3500 ;
-const ADMIN ="Admin"
-
+const PORT = process.env.PORT || 3500
+const ADMIN = "Admin"
 
 const app = express()
 
 app.use(express.static(path.join(__dirname, "public")))
 
 const expressServer = app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
+    console.log(`listening on port ${PORT}`)
 })
 
 // state 
 const UsersState = {
     users: [],
-    setUsers: function(newUsersArray){
-        this.users = newUsersArray;
+    setUsers: function (newUsersArray) {
+        this.users = newUsersArray
     }
-} 
+}
 
-const io = new Server(expressServer , {
+const io = new Server(expressServer, {
     cors: {
-        // if we are not hosting the backend on a different domain then the front end , we need to add the front end production URL 
-        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500","http://127.0.0.1:5500"]
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
     }
 })
 
-io.on('connection', socket =>{
-    // print the id of the connected user 
+io.on('connection', socket => {
     console.log(`User ${socket.id} connected`)
-    
-    // Upon connection - only to user
-    socket.emit('message', buildMsg(ADMIN , "Welcome to Chat App!"))
 
-    socket.on('enterRoom', ({name ,room}) =>{
-        //leave previous room
+    // Upon connection - only to user 
+    socket.emit('message', buildMsg(ADMIN, "Welcome to Chat App!"))
+
+    socket.on('enterRoom', ({ name, room }) => {
+
+        // leave previous room 
         const prevRoom = getUser(socket.id)?.room
+
+        if (prevRoom) {
+            socket.leave(prevRoom)
+            io.to(prevRoom).emit('message', buildMsg(ADMIN, `${name} has left the room`))
+        }
+
+        const user = activateUser(socket.id, name, room)
+
+        // Cannot update previous room users list until after the state update in activate user 
+        if (prevRoom) {
+            io.to(prevRoom).emit('userList', {
+                users: getUsersInRoom(prevRoom)
+            })
+        }
+
+        // join room 
+        socket.join(user.room)
+
+        // To user who joined 
+        socket.emit('message', buildMsg(ADMIN, `You have joined the ${user.room} chat room`))
+
+        // To everyone else 
+        socket.broadcast.to(user.room).emit('message', buildMsg(ADMIN, `${user.name} has joined the room`))
+
+        // Update user list for room 
+        io.to(user.room).emit('userList', {
+            users: getUsersInRoom(user.room)
+        })
+
+        // Update rooms list for everyone 
+        io.emit('roomList', {
+            rooms: getAllActiveRooms()
+        })
     })
 
-    //Upon connection - to all others
-    socket.broadcast.emit('message', `User ${socket.id.substring(0,5)} connected`)
+    // When user disconnects - to all others 
+    socket.on('disconnect', () => {
+        const user = getUser(socket.id)
+        userLeavesApp(socket.id)
 
-    socket.on('message', data =>{
-        console.log(data);
-        io.emit('message' ,`${socket.id.substring(0,5)}: ${data}`); // send the first 5 chars of the id of the client + the data
+        if (user) {
+            io.to(user.room).emit('message', buildMsg(ADMIN, `${user.name} has left the room`))
+
+            io.to(user.room).emit('userList', {
+                users: getUsersInRoom(user.room)
+            })
+
+            io.emit('roomList', {
+                rooms: getAllActiveRooms()
+            })
+        }
+
+        console.log(`User ${socket.id} disconnected`)
     })
 
-    //when user disconnects - to all others 
-    socket.on('disconnect', ()=> {
-        socket.broadcast.emit('message', `User ${socket.id.substring(0,5)} disconnected`)
+    // Listening for a message event 
+    socket.on('message', ({ name, text }) => {
+        const room = getUser(socket.id)?.room
+        if (room) {
+            io.to(room).emit('message', buildMsg(name, text))
+        }
     })
 
-    //Listen for activity 
-    socket.on('activity',(name) =>{
-        socket.broadcast.emit('activity' , name);
+    // Listen for activity 
+    socket.on('activity', (name) => {
+        const room = getUser(socket.id)?.room
+        if (room) {
+            socket.broadcast.to(room).emit('activity', name)
+        }
     })
-
-
 })
 
-
-function buildMsg(name,text){
+function buildMsg(name, text) {
     return {
         name,
         text,
-        time: new Intl.DateTimeFormat('default',{
+        time: new Intl.DateTimeFormat('default', {
             hour: 'numeric',
             minute: 'numeric',
             second: 'numeric'
@@ -80,31 +126,29 @@ function buildMsg(name,text){
 }
 
 // User functions 
-function activateUser(id , name, room){
-    const user = { id , name , room}
+function activateUser(id, name, room) {
+    const user = { id, name, room }
     UsersState.setUsers([
         ...UsersState.users.filter(user => user.id !== id),
-        user 
+        user
     ])
-    return user ;
+    return user
 }
 
-function userLeavesApp(id){
+function userLeavesApp(id) {
     UsersState.setUsers(
         UsersState.users.filter(user => user.id !== id)
     )
 }
 
-function getUser(id){
+function getUser(id) {
     return UsersState.users.find(user => user.id === id)
 }
 
-function getUsersInRoom(room){
+function getUsersInRoom(room) {
     return UsersState.users.filter(user => user.room === room)
 }
 
-function getAllActiveRooms(){
-    return Array.from(new Set(UserState.users.map(user => user.room)))
+function getAllActiveRooms() {
+    return Array.from(new Set(UsersState.users.map(user => user.room)))
 }
-
-
